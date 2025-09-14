0080eb4af4900f8a635a4ae49aa3f87f8f46dd5b
Paloma Carvalho <paloma.clima@gmail.com>
2025-09-14 19:35:30 -0400

    style: Melhora legibilidade e cores do formulário de contato

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
 
         <header className="bg-blue-600 text-white text-center py-8 shadow-md">
-            <h1 className="text-3xl font-bold">InfoTech Solutiuons</h1>
+            <h1 className="text-3xl font-bold">InfoTech Solutions</h1>
             <p className="text-lg mt-2">
                 Desenvolvimento de Software & Consultoria em Segurança da Informação
             </p>
